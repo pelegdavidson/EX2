b31e7efb843e24b832ad7ad0a9467832bf3d2f1b
pelegdavidson <pelegd13@gmail.com>
2017-11-04 17:10:00 +0200

    ex2

--- app/src/main/java/com/example/peleg/ex2/MainActivity.java
+++ app/src/main/java/com/example/peleg/ex2/MainActivity.java
@@ -10,54 +10,84 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    private int cnt;
-    final String MY_TAG="EX2.MainActivity";
+    private static int cnt;
+    private Boolean showToast=false;
+    final String MY_TAG = "EX2.MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(MY_TAG, "On Create Event");
-        Toast.makeText(this, "hello this is your java coding experience", Toast.LENGTH_LONG).show();
+        Log.i(MY_TAG, getString(R.string.create));
+        if(savedInstanceState==null){
+            Toast.makeText(this, getString(R.string.introMessage), Toast.LENGTH_LONG).show();
+        }
+        else{
+            if(showToast){
+                Toast.makeText(this, getString(R.string.introMessage), Toast.LENGTH_LONG).show();
+            }
+        }
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(MY_TAG, "On Start Event");
+        Log.i(MY_TAG, getString(R.string.start));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(MY_TAG, "On Resume Event");
+        Log.i(MY_TAG, getString(R.string.resume));
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(MY_TAG, "On Pause Event");
+        Log.i(MY_TAG, getString(R.string.pause));
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(MY_TAG, "On Stop Event");
+        Log.i(MY_TAG, getString(R.string.stop));
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(MY_TAG, "On Destroy Event");
+        Log.i(MY_TAG, getString(R.string.destroy));
     }
-    public void buttonClicked(View v){
+
+    public void buttonClicked(View v) {
 
         EditText edName = (EditText) findViewById(R.id.input);
-        if(edName.getText().length()==0){
-            Toast.makeText(this, "you must enter your name before clicking", Toast.LENGTH_LONG).show();
-        }
-        else{
+        if (edName.getText().length() == 0) {
+            Toast.makeText(this, getString(R.string.notClicked), Toast.LENGTH_LONG).show();
+        } else {
             cnt++;
-            Toast.makeText(this, edName.getText()+", you clicked "+cnt+" times", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, edName.getText() + getString(R.string.clickCnt1) + cnt + getString(R.string.clickCnt2), Toast.LENGTH_LONG).show();
         }
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+
+        savedInstanceState.putInt("MyInt", cnt);
+        savedInstanceState.putBoolean("Toast_shown", true);
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+
+        super.onRestoreInstanceState(savedInstanceState);
+
+        int myInt = savedInstanceState.getInt("MyInt");
+        boolean myBoolean = savedInstanceState.getBoolean("Toast_shown");
+        this.showToast=!myBoolean;
+    }
+
 }
